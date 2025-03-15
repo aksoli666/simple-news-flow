efe3eae8e4b5844ffda434298dceec78bc2dc2eb
aksoli666 <olexsandradorofeieva@gmail.com>
2025-03-16 00:55:43 +0100

    Initial use of Api, Model from resp and Retrofit - added model from api response - set retrofit

---
+++ app/src/main/java/com/example/simplenewsflow/model/ResponseNews.kt
@@ -0,0 +1,6 @@
+package com.example.simplenewsflow.model
+
+data class ResponseNews(
+    val sources: List<Source>,
+    val status: String
+)
---
+++ app/src/main/java/com/example/simplenewsflow/util/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.simplenewsflow.util
+
+class Constant {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "9bc109457e504abf8879756f81e3782b"
+    }
+}
